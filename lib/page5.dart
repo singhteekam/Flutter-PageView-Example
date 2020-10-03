757e44cd622469d125cb3d063523359cbc296f21
dynamicdevs404 <72301587+dynamicdevs404@users.noreply.github.com>
2020-10-03 16:21:54 +0530

    Create page5.dart

---
+++ lib/page5.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Page5 extends StatelessWidget {
+
+  @override
+
+  Widget build(BuildContext context) {
+
+    return Center(
+
+      child: Column(
+
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        children:<Widget>[
+
+          Text("Page 5",textAlign: TextAlign.center,style: TextStyle(fontSize: 30,fontWeight:FontWeight.bold),),
+
+          
+
+        ]
+
+      ),
+
+    );
+
+  }
+
+}
